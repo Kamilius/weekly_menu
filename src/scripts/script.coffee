18cd55ce89824ca2b1dab18bb9100d8c299a523f
Oleksandr Hutsulyak <kamiklviv@gmail.com>
2014-10-30 16:51:58 +0200

    Implementing DnD functionality in progress

--- src/scripts/script.coffee
+++ src/scripts/script.coffee
@@ -19,6 +19,30 @@ app.controller 'RecipesCtrl', ['$scope', ($scope) ->
 		new Recipe 'Steak', ['beef meat', 'spice']
 		new Recipe 'Baked potato', ['potato', 'spice']
 		new Recipe 'Steak', ['beef meat', 'spice']
+		new Recipe 'Baked potato', ['potato', 'spice']
+		new Recipe 'Steak', ['beef meat', 'spice']
+		new Recipe 'Baked potato', ['potato', 'spice']
+		new Recipe 'Steak', ['beef meat', 'spice']
+		new Recipe 'Baked potato', ['potato', 'spice']
+		new Recipe 'Steak', ['beef meat', 'spice']
+		new Recipe 'Baked potato', ['potato', 'spice']
+		new Recipe 'Steak', ['beef meat', 'spice']
+		new Recipe 'Baked potato', ['potato', 'spice']
+		new Recipe 'Steak', ['beef meat', 'spice']
+		new Recipe 'Baked potato', ['potato', 'spice']
+		new Recipe 'Steak', ['beef meat', 'spice']
+		new Recipe 'Baked potato', ['potato', 'spice']
+		new Recipe 'Steak', ['beef meat', 'spice']
+		new Recipe 'Baked potato', ['potato', 'spice']
+		new Recipe 'Steak', ['beef meat', 'spice']
+		new Recipe 'Baked potato', ['potato', 'spice']
+		new Recipe 'Steak', ['beef meat', 'spice']
+		new Recipe 'Baked potato', ['potato', 'spice']
+		new Recipe 'Steak', ['beef meat', 'spice']
+		new Recipe 'Baked potato', ['potato', 'spice']
+		new Recipe 'Steak', ['beef meat', 'spice']
+		new Recipe 'Baked potato', ['potato', 'spice']
+		new Recipe 'Steak', ['beef meat', 'spice']
 	]
 
 	recipesContainer = document.querySelector('.recipes-container')
@@ -27,13 +51,24 @@ app.controller 'RecipesCtrl', ['$scope', ($scope) ->
 	recipesContainer.addEventListener 'dragstart', (event) ->
 		el = event.target
 		if el.classList[0] == 'recipe' then event.target.style.opacity = '0.4'
+	, false
 	calendar.addEventListener 'dragenter', (event) ->
 		el = event.target
 		if el.classList[1] == 'day' then el.classList.add('over')
+	, false
 	calendar.addEventListener 'dragleave', (event) ->
 		el = event.target
-		if el.classList[1] == 'day' then el.classList.remove('over')
+		if el.classList[1] == 'day' then el.classList.remove('over')	
+	, false
 ]
 
 app.controller 'CalendarCtrl', ['$scope', ($scope) ->
+	$scope.weeklyMenu = {}
+	calendar = document.querySelector('.calendar-recipes')
+	calendar.addEventListener 'drop', (event) ->
+		draggable = event.target
+		droppable = this
+		if droppable.classList[1] == 'day' then $scope.weeklyMenu[droppable.classList[2]].push JSON.parse(event.dataTransfer.getData('json'))
+		console.log($scope.weeklyMenu)
+	, false
 ]
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -15,7 +15,7 @@ Recipe = (function() {
 app.controller('RecipesCtrl', [
   '$scope', function($scope) {
     var calendar, recipesContainer;
-    $scope.recipes = [new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice'])];
+    $scope.recipes = [new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice']), new Recipe('Baked potato', ['potato', 'spice']), new Recipe('Steak', ['beef meat', 'spice'])];
     recipesContainer = document.querySelector('.recipes-container');
     calendar = document.querySelector('.calendar-recipes');
     recipesContainer.addEventListener('dragstart', function(event) {
@@ -24,22 +24,37 @@ app.controller('RecipesCtrl', [
       if (el.classList[0] === 'recipe') {
         return event.target.style.opacity = '0.4';
       }
-    });
+    }, false);
     calendar.addEventListener('dragenter', function(event) {
       var el;
       el = event.target;
       if (el.classList[1] === 'day') {
         return el.classList.add('over');
       }
-    });
+    }, false);
     return calendar.addEventListener('dragleave', function(event) {
       var el;
       el = event.target;
       if (el.classList[1] === 'day') {
         return el.classList.remove('over');
       }
-    });
+    }, false);
   }
 ]);
 
-app.controller('CalendarCtrl', ['$scope', function($scope) {}]);
+app.controller('CalendarCtrl', [
+  '$scope', function($scope) {
+    var calendar;
+    $scope.weeklyMenu = {};
+    calendar = document.querySelector('.calendar-recipes');
+    return calendar.addEventListener('drop', function(event) {
+      var draggable, droppable;
+      draggable = event.target;
+      droppable = this;
+      if (droppable.classList[1] === 'day') {
+        $scope.weeklyMenu[droppable.classList[2]].push(JSON.parse(event.dataTransfer.getData('json')));
+      }
+      return console.log($scope.weeklyMenu);
+    }, false);
+  }
+]);
