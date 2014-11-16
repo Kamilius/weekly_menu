80062fbc25706fbf79fc2a00e7042288b99bffc8
Kamilius <kamiklviv@gmail.com>
2014-11-16 22:50:51 +0200

    Added mongodb database support

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    mongo = require('mongodb'),
+    monk = require('monk'),
+    db = monk('localhost:27017/nodetest1');
+
+app.use(function(req, res, next) {
+  req.db = db;
+  next();
+});
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/build/index.html');
+});
+
+app.get('/users', function(req, res) {
+  var db = req.db,
+      collection = db.get('usercollection');
+
+  collection.find({}, {}, function(e, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
 });
 
 app.use(express.static(__dirname + '/build'));
